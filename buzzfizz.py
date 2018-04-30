c81ed099356100f172252fdd057863280b787742
rpates <36613728+rpates@users.noreply.github.com>
2018-04-29 21:15:45 -0400

    Update buzzfizz.py

--- buzzfizz.py
+++ buzzfizz.py
@@ -7,3 +7,6 @@ for num in range(1,101):
 	if num % 5 != 0 and num % 3 != 0:
 		string = string + str(num)
 	print(string)
+	
+# The code shown is a FizzBuzz program code. It shows that multiples of 15 print as 'FizzBuzz' , multiples of 5 print as 'Buzz' , and multiples of 3 print as 'Fizz'. 
+# People can use this simple code to not only check if their program is running correctly, but to also practice functions such as Variables and constants, Operators, and Control structures.
